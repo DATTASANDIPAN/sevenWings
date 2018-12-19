25c37a7a6f93299c9fe6a2da551c7f4a430d8ebf
dattasandipan <sandipandatta10@gmail.com>
2018-12-20 00:14:56 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,293 @@
+<html>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+         <?php $n=$_GET["n"]; ?>
+    
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+    <title>Merry Christmas Greetings with Name</title>
+    <meta name="google" value="notranslate">
+    <meta property="og:type" content="Greetings" />
+    <meta property="og:title" content="Merry Christmas Greetings with Name" />
+    <meta property="og:url" content="http://Yoursite.com">
+    <meta property="og:description" content="Xmas 2018 - Sent Animated & beautiful Merry Christmas greeting card with your name to friends & relatives" />
+    <meta property="og:site_name" content="Yoursite.com" />
+    <meta property="og:image" content="img/gift.webp" />
+    <meta property="og:image:secure_url" content="img/gift.webp" />
+    <meta property="og:image:width" content="993" />
+    <meta property="og:image:height" content="559" />
+    <meta property="og:image:alt" content="Merry Christmas Greetings with Name" />
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link href='https://fonts.googleapis.com/css?family=Sofia:&effect=neon' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/wp.webp">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script>
+ if ('serviceWorker' in navigator) {
+
+    console.log("Will the service worker register?");
+
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(reg){
+
+        console.log("Yes, it did.");
+
+     }).catch(function(err) {
+
+        console.log("No it didn't. This happened:", err)
+
+    });
+ }
+
+
+</script>
+</head>
+<iframe width="0" height="0" src="song.mp3" frameborder="0" allowfullscreen></iframe>
+<body class="bg" id="rakhi" style="background-image: linear-gradient(to bottom, #ff0000, #ff517f, #ff97d2, #f8d1fc, #ffffff););
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;">
+
+<center><img src="https://goo.gl/xvjgXx" width="100%" height="12%" alt="A very merry Christmas and happy new year 2019"></center>
+<div class="w3-bar">
+  <button class="w3-bar-item w3-button w3-black" style="width:33.3%"><a href="pages/about.php">About</a></button>
+  <button class="w3-bar-item w3-button w3-teal" style="width:33.3%"><a href="Yoursite.com">Home</a></button>
+  <button class="w3-bar-item w3-button w3-blue" style="width:33.3%"><a href="pages/tos.php">TOS</a></button>
+</div>
+<center>
+<!---------Adsense Link Ads-------->
+
+</center>
+<div class="container">
+  <marquee class="m1" behavior="scroll" direction="up" scrolldelay="5">  <br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+
+</marquee>
+<marquee class="m2" behavior="scroll" direction="down" scrolldelay="5"><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+<img src="https://goo.gl/sjadWz" height="119px" width="35px"/><br><br>
+</marquee>
+<script src="js/doodle.js"></script> 
+<script type="text/javascript">
+ doodle.init("img/fly.webp");
+</script>
+<div class="container" >
+    <div class="main-greeting" >
+<div align="center html2canvas-ignore">
+     <div style="font-size: 17px; font-weight: 800; color: white;" >
+<center>
+    <p id="demo"></p></center>
+
+            <div class="main_body" >
+      <center>
+<figure>
+    
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+<h1 class="naming">                 
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</h1>
+
+
+</figure><br> <br><br><br>
+</center>
+<p id="demo"></p> 
+<center>
+<img src="https://i.imgur.com/DPMaeP8.png" width="70%" height="13%"style="animation: tada 4s infinite"><br>
+<img src="img/santa.webp" width="350px" height="400px"style="animation: pulse 1.5s infinite"><br><br>
+</center>
+<center>
+<span class="hny-txt" style="color: #000">
+<span style="font-size: 28px;">Merry Christmas</span><br />
+
+Me: “I want a magical Unicorn for Christmas”<br />
+Santa: “Be realistic”<br />
+Me: “Okay. I want 5 minutes to myself each day to drink my coffee hot and pee in peace.”<br />
+Santa: “What color Unicorn would you like?”<br />
+This made me think of us! Thank you for being such a great friend and support this year. Wishing you more sleep and time for yourself in the coming year. Happy New Year!<br />
+</span></p>
+<img src="https://goo.gl/bKt84W"  class="swing2"  height="300px" width="300px"/>
+</center>
+<center>
+<p class="hny-txt">
+  <span style="font-size: 32px;"><b>X-Mas Gift By</span> <span style="color: #008000;"></span></p></center>&nbsp;
+<br>
+<center>
+   <!-- Display the countdown timer in an element -->
+
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("dec 25, 2018 00:00:00").getTime();
+
+// Update the count down every 01 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "<i> Days,</i> " + hours + " <i>Hrs,</i> "
+  + minutes + "<i> Min,</i> " + seconds + "<i> Sec</i> ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Merry Christmas";
+  }
+}, 1000);
+</script> 
+</div>
+<center>
+
+<div class="button">
+    
+                
+<?php
+if($n==null)
+
+ echo "[Your Name]";
+    else
+    echo $_GET["n"];
+?>
+</div></center><br>
+<center>
+<!---------Adsense Link Ads-------->
+
+</center>
+<center>
+<div class="w3-bar">
+  <button class="w3-bar-item w3-button w3-black" style="width:33.3%"><a href="pages/about.php">About</a></button>
+  <button class="w3-bar-item w3-button w3-teal" style="width:33.3%"><a href="Yoursite.com">Home</a></button>
+  <button class="w3-bar-item w3-button w3-red" style="width:33.3%"><a href="pages/tos.php">TOS</a></button>
+</div>
+</center>
+<center>
+<span style="font-size: 7pt; color: #000000;"><p>Is the No. one Festival Wishing site right now, here you can get many international Festival wishing Greeting card templates, which will help you to create an awesome looking Greeting card with your own name, what you will need to do? just Enter your name in the below box and Click on Go button, on the next page you will see your name billing on top, now just Click on the Share button to share the greetings with your Family, Friends, and relatives. Thank You!</p>
+Copyrights © 2018 | All Rights Reserved Powered by <a href="https://www.ripon.in">RipoN</a>
+</span>
+</center>
+<br><br>
+</div>
+</div>
+</div>
+</div>
+</div>
+<form method="get" action="share.php">
+  <div class="enter-name">
+    <input class="animated pulse infinite" type="name" required="" maxlength="50" name="n" placeholder="Enter Your Name Here">
+    <button class="btn animated shake infinite" type="submit"> Go</button>
+  </div>
+</form>       
+<canvas id="canvas" style="position : absolute; top : 0px; left : 0px;"></canvas>
+    </body>
+    <link rel="manifest" href="js/manifest.json">
+</html> 
+
---
+++ pages/tos.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Terms of Service and Privacy Policy | YourSite.com</title>
+	<meta property="og:type" content="page" />
+    <meta property="og:title" content="Terms of Service and Privacy Policy | YourSite.com" />
+    <meta property="og:url" content="http://Yoursite.com">
+    <meta property="og:description" content="Terms of Service and Privacy Policy Yoursite.com" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" sizes="32x32" href="http://Yoursite.com/img/wp.webp">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+<center><img src="https://goo.gl/xvjgXx" width="100%" height="8%" alt="A very merry Christmas and happy new year 2019"></center>
+<div class="w3-bar">
+  <button class="w3-bar-item w3-button w3-black" style="width:33.3%"><a href="about.php">About</a></button>
+  <button class="w3-bar-item w3-button w3-teal" style="width:33.3%"><a href="Yoursite.com">Home</a></button>
+  <button class="w3-bar-item w3-button w3-red" style="width:33.3%"><a href="tos.php">TOS</a></button>
+</div>
+
+<h2 style="text-align: center;">Privacy Policy</h2>
+<hr />
+<section class="terms-content">We take your privacy seriously. This policy outlines our information gathering, cookie policy, and privacy-related practices for YourSite. It also overviews information related to private information as stated in the GDPR which went into effect on May 25, 2018.
+<p style="text-align: center;"><strong>Processing Personal Data</strong></p>
+<p style="text-align: center;">YourSite.com is open and easy to use platform; no user login is offered on our website, we don’t set any first-party cookies, and we don’t track users in any personally identifiable way.</p>
+<p style="text-align: center;">We automatically receive information from your web browser or mobile device. This information includes the name of the website from which you entered our website, if any, as well as the name of the website you’ll visit when you leave our website. This information also includes the IP address of your computer/the proxy server you use to access the Internet, your Internet service provider’s name, your web browser type, the type of mobile device, your computer operating system, and data about your browsing activity when using our website including what is searched for. We use this information to analyze trends among our users and to help improve our website. We do not use any personally identifiable information which could be attained by analysis of our log files, and our platform doesn't store any data or personally identifiable information about our users.</p>
+<p style="text-align: center;"><strong>Cookies</strong></p>
+<p style="text-align: center;">YourSite does not use cookies to deliver content specific to your interests, and we don't set any first-party cookies to help deliver more targeted ads.</p>
+<p style="text-align: center;"><strong>External Links</strong></p>
+<p style="text-align: center;">YourSite contains links to other sites. YourSite is not responsible for the privacy practices or the content of the linked websites.</p>
+<p style="text-align: center;"><strong>Advertising</strong></p>
+<p style="text-align: center;">We use third-party advertising companies to serve ads when you visit YourSite.com. These advertising networks usually place cookies to develop a profile of your browsing interests and serve advertisements on other websites that are related to your interests. You will see less advertising if you disable these cookies. You can visit the <a href="http://optout.networkadvertising.org/?c=1#!/">Network Advertising Initiative</a> For more information about this practice and your possibility to opt-out.</p>
+<p style="text-align: center;"><strong>Third-Party Privacy Policies</strong></p>
+<p style="text-align: center;">Third-party vendors use cookies to serve ads based on a user's prior visits to our website and/or other websites.</p>
+<p style="text-align: center;">Here is a list of each third-party advertising vendor we work with along with a link to their privacy policy.</p>
+
+<ol>
+ 	<li><a href="https://www.google.com/policies/privacy/partners/">Google Privacy</a></li>
+ 	<li><a href="https://www.awio.com/privacy">Awio</a></li>
+ 	<li><a href="https://policies.google.com/privacy">Google DFP/AdX</a></li>
+ 	<li><a href="https://www.facebook.com/policy.php">Facebook</a></li>
+</ol>
+<div style="text-align: center;"><strong>Google Analytics Privacy Notice</strong></div>
+<div style="text-align: center;">
+
+Our website uses Google Analytics to collect information about the use of our website. Google Analytics collects information from users such as how often they visit our website, what pages they visit, and what other websites they have used before coming to our website. We use the information we get from Google Analytics to analyze traffic and to improve our website. Google Analytics collects only the IP address assigned to you on the date you visit our website, not your name or other identifying information. We do not combine the information collected using Google Analytics with any personally identifiable information. Although Google Analytics plants a permanent cookie on your web browser to identify you as a unique user the next time you visit our website, the cookie cannot be used by anyone but Google. Google also uses specific identifiers to help collect information about the use of our website. For more information on how Google collects and processes your data, visit <a href="https://www.google.com/policies/privacy/partners/">Google's Privacy And Terms</a>. You can prevent Google Analytics from using your information by <a href="https://tools.google.com/dlpage/gaoptout">Opting-Out</a>.
+
+</div>
+<p style="text-align: center;"><strong>GDPR</strong></p>
+<p style="text-align: center;">European and UK residents, as outlined in the GDPR have the following rights:</p>
+<p style="text-align: center;">The right to be informed about personal data we collect and how we process it</p>
+<p style="text-align: center;">The right to access your personal data</p>
+<p style="text-align: center;">The right to correct your personal data if it is inaccurate</p>
+<p style="text-align: center;">The right to request removal of your personal data if there is no compelling reason for us to continue to process it</p>
+<p style="text-align: center;">For additional rights and additional information on your rights, we encourage you to visit this <a href="https://ico.org.uk/for-organisations/guide-to-the-general-data-protection-regulation-gdpr">Guide To The GDRP</a>.</p>
+<p style="text-align: center;"><strong>Data Controller Contact</strong></p>
+<p style="text-align: center;">If you have any questions about this privacy statement, the practices of this website, or your dealings with this website, please contact:</p>
+<p style="text-align: center;"><a href="mailto:Admin@youremail.com">Admin@youremail.com</a></p>
+</section>
+<div class="w3-bar">
+  <button class="w3-bar-item w3-button w3-black" style="width:33.3%"><a href="about.php">About</a></button>
+  <button class="w3-bar-item w3-button w3-teal" style="width:33.3%"><a href="Yoursite.com">Home</a></button>
+  <button class="w3-bar-item w3-button w3-red" style="width:33.3%"><a href="tos.php">TOS</a></button>
+</div>
+<center>
+  Copyrights © 2018 | All Rights Reserved by <a href="https://www.ripon.in">RipoN</a>
+</center>
+</body>
+</html>
